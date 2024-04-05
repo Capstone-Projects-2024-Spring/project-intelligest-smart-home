838b5f4112c7dd5d60e01f715df87fc77bcd89b6
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-04-05 13:01:48 -0400

    added 8 buttons

--- intelligest-ui/src/app/page.js
+++ intelligest-ui/src/app/page.js
@@ -10,7 +10,28 @@ export default function Home() {
       <div className="bg-gray-200 min-h-screen flex justify-center items-center">
         <div className="grid grid-cols-4 gap-4">
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            Button
+            TV
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Lights
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Alarm
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Weather
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Thermostat
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Locks
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Reminders
+          </button>
+          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            To-do List
           </button>
         </div>
       </div>
