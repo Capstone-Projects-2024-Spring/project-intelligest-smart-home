29dda42514e820b01811a45f1ff6668ef9b120ed
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-04-09 13:01:33 -0400

    adding image to button background

--- intelligest-ui/src/app/page.js
+++ intelligest-ui/src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import tv from "./TV.png"
 
 export default function Home() {
     const handleClick = (buttonName) => {
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="bg-gray-200 min-h-screen flex justify-center items-center">
         <div className="grid grid-cols-4 gap-4">
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={tv} alt="TV" width={50} height={50} />
             TV
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
