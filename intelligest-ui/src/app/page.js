0a7a504b05e26c3705b5792b6941f2fc7a0768d7
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-04-09 14:11:48 -0400

    changed bg of buttons

--- intelligest-ui/src/app/page.js
+++ intelligest-ui/src/app/page.js
@@ -18,35 +18,35 @@ export default function Home() {
       <main className="flex min-h-screen flex-col">
       <div className="bg-gray-200 min-h-screen flex justify-center items-center">
         <div className="grid grid-cols-4 gap-4">
-          <button className="hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={tv} alt="TV" width={140} height={50} />
             TV
           </button>
-          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={light} alt="TV" width={140} height={50} />
             Lights
           </button>
-          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={alarm} alt="TV" width={140} height={50} />
             Alarm
           </button>
-          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={locks} alt="TV" width={140} height={50} />
             Weather
           </button>
-          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={reminders} alt="TV" width={140} height={50} />
             Thermostat
           </button>
-          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={thermostat} alt="TV" width={140} height={50} />
             Locks
           </button>
-          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={weather} alt="TV" width={140} height={50} />
             Reminders
           </button>
-          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={toDo} alt="TV" width={140} height={80} />
             To-do List
           </button>
