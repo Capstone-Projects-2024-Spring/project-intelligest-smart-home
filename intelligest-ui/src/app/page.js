b2cf5419f24317874f34ec1abdc32da47af35289
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-04-09 13:55:31 -0400

    perfectly designed button background with gesture images

--- intelligest-ui/src/app/page.js
+++ intelligest-ui/src/app/page.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import tv from "./TV.png";
 import light from "./lights.png";
@@ -8,8 +9,6 @@ import thermostat from "./thermostat.png"
 import weather from "./weather.png"
 import toDo from "./to-dolist.png"
 
-
-
 export default function Home() {
     const handleClick = (buttonName) => {
     console.log(buttonName);
@@ -19,36 +18,36 @@ export default function Home() {
       <main className="flex min-h-screen flex-col">
       <div className="bg-gray-200 min-h-screen flex justify-center items-center">
         <div className="grid grid-cols-4 gap-4">
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={tv} alt="TV" width={50} height={50} />
+          <button className="hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={tv} alt="TV" width={140} height={50} />
             TV
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={light} alt="TV" width={50} height={50} />
+          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={light} alt="TV" width={140} height={50} />
             Lights
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={alarm} alt="TV" width={50} height={50} />
+          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={alarm} alt="TV" width={140} height={50} />
             Alarm
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={locks} alt="TV" width={50} height={50} />
+          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={locks} alt="TV" width={140} height={50} />
             Weather
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={reminders} alt="TV" width={50} height={50} />
+          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={reminders} alt="TV" width={140} height={50} />
             Thermostat
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={thermostat} alt="TV" width={50} height={50} />
+          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={thermostat} alt="TV" width={140} height={50} />
             Locks
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={weather} alt="TV" width={50} height={50} />
+          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={weather} alt="TV" width={140} height={50} />
             Reminders
           </button>
-          <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            <Image src={toDo} alt="TV" width={50} height={50} />
+          <button className="custom-image hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={toDo} alt="TV" width={140} height={80} />
             To-do List
           </button>
         </div>
