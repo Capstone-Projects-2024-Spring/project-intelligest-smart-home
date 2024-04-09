aef462b5c50e9e7305b6c5cf0958d1adde480428
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-04-09 13:10:16 -0400

    minor addition

--- intelligest-ui/src/app/page.js
+++ intelligest-ui/src/app/page.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
-import tv from "./TV.png"
+import tv from "./TV.png";
+import light from "./lights.png";
+import alarm from "./alarm.png";
+import locks from "./locks.png";
+import reminders from "./reminders.png"
+import thermostat from "./thermostat.png"
+import weather from "./weather.png"
+import toDo from "./to-dolist.png"
+
+
 
 export default function Home() {
     const handleClick = (buttonName) => {
@@ -15,24 +24,31 @@ export default function Home() {
             TV
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={light} alt="TV" width={50} height={50} />
             Lights
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={alarm} alt="TV" width={50} height={50} />
             Alarm
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={locks} alt="TV" width={50} height={50} />
             Weather
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={reminders} alt="TV" width={50} height={50} />
             Thermostat
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={thermostat} alt="TV" width={50} height={50} />
             Locks
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={weather} alt="TV" width={50} height={50} />
             Reminders
           </button>
           <button className="bg-yellow-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            <Image src={toDo} alt="TV" width={50} height={50} />
             To-do List
           </button>
         </div>
