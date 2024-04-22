90397a24cd8bfa784cbdd00206fd348ac9c1303a
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-04-21 20:31:18 -0400

    minor addition

--- intelligest-ui/testing-react-app/src/App.js
+++ intelligest-ui/testing-react-app/src/App.js
@@ -4,22 +4,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="IntelliGest UI">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
         >
-          Learn React
-        </a>
+          Video Feed
+        </button>
+        <button>
+          Home Assistant Icon
+        </button>
+        <button>
+          TV Gesture Button
+        </button>
+        <button>
+          Light Gesture Button
+        </button>
+        <button>
+          Alarm Gesture Button
+        </button>
+        <button>
+          Weather Gesture Button
+        </button>
+        <button>
+          Thermostat Gesture Button
+        </button>
+        <button>
+          Locks Gesture Button
+        </button>
+        <button>
+          Reminders Gesture Button
+        </button>
+        <button>
+          To-do List Gesture Button 
+        </button>
       </header>
+      <div></div>
+
+
     </div>
   );
 }
 
 export default App;
+
