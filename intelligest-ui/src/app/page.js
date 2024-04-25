891d7e7d249f7f8148ad69fcaf2f12396ce1c809
defining-art <118932320+defining-art@users.noreply.github.com>
2024-04-25 10:29:16 -0400

    Add To-Do List

--- intelligest-ui/src/app/page.js
+++ intelligest-ui/src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import tv from "./gesture-imgs/TV.png";
 import light from "./gesture-imgs/lights.png";
@@ -10,11 +10,23 @@ import thermostat from "./gesture-imgs/thermostat.png";
 import weather from "./gesture-imgs/weather.png";
 import toDo from "./gesture-imgs/to-dolist.png";
 import Icon from '@mdi/react';
-import { mdiAccount, mdiAccountMultiple, mdiHomeAssistant } from '@mdi/js';
+import { mdiAccount, mdiAccountMultiple, mdiFlaskEmpty, mdiHomeAssistant } from '@mdi/js';
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   const [data, setData] = useState({}); // Declare 'data' in your component's state
+  const [lightState, setLightState] = useState("Inactive");
+  const [lockState, setLockState] = useState("Inactive");
+  const [thermostatState, setThermostatState] = useState("Inactive");
+  const [tvState, setTvState] = useState("Inactive");
+
+  const [viewToDoList, setViewToDoList] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
   const handleClick = (buttonName) => {
     console.log(buttonName);
   };
@@ -40,6 +52,50 @@ export default function Home() {
       eventSource.close();      
     };
   }, []);
+
+  //Capture the change of states in devices.
+  useEffect(() => {
+    switch(data.deviceChoice){
+      case 'Light':
+        (data.deviceStatus == 'on') && (setLightState("Active"));
+      case 'Lock':
+        (data.deviceStatus == 'on') && (setLockState("Active"));
+      case 'Thermostat':
+        (data.deviceStatus == 'on') && (setThermostatState("Active"));
+      case 'TV':
+        (data.deviceStatus == 'on') && (setTvState("Active"));
+      default:
+        console.log("Error");
+    }
+  },[data.lastestGesture]);
+
+  const getToDoList=()=>{
+    console.log("Enetered");
+    setViewToDoList(true);
+  }
+
+  const closeToDoList=()=>{
+    setViewToDoList(false);
+  }
+  
+  function handleInputChange(event){
+    setNewTask(event.target.value);
+    console.log("Task Set");
+  }
+
+  function addTask(){
+    console.log("Adding Task");
+    if(newTask.trim() !== ""){
+        setTasks(t => [...t, newTask]);
+        setNewTask("");
+    }
+    console.log("Task Added");
+}
+
+function deleteTask(index){
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+}
  
   return (
     <main className="flex min-h-screen flex-col">
@@ -59,6 +115,17 @@ export default function Home() {
             Device Status: {data.deviceStatus} <br />
           </div>
         </div>
+
+        <div style={{ color:"gray",position:"absolute", top:"5px", right:"5px"}}>Devices 
+          <select style={{border:"black"}}>
+            <option label=" "> </option>
+            <option>Light : {lightState} </option>
+            <option>Lock : {lockState}</option>
+            <option>Thermostat : {thermostatState}</option>
+            <option>TV : {tvState}</option>
+          </select>
+        </div>
+
         <div data-testid="button-test" className="grid grid-cols-4 gap-4">
           <button onClick={() => handleClick("TV Button Pressed")} className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={tv} alt="TV" width={140} height={50} />
@@ -92,12 +159,32 @@ export default function Home() {
             <Image src={weather} alt="TV" width={140} height={50} />
             Reminders
           </button>
-          <button onClick={() => handleClick("To-do Button Pressed")} className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
+          <button onClick={()=>getToDoList()} className="hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
             <Image src={toDo} alt="TV" width={140} height={80} />
             To-do List
           </button>
         </div>
       </div>
+
+      <Modal show={viewToDoList} onHide={closeToDoList}>
+        <Modal.Header closeButton>
+        <Modal.Title>To-Do List</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <input type="text" placeholder="Enter an task..." style={{border:"1px solid black", borderRadius: "4px"}} value={newTask} onChange={handleInputChange}/>
+            <button onclick={addTask}>Add</button>
+          </div>
+          <ul>
+            {tasks.map((task, index)=> 
+            <li key={index}>
+              {task}
+              <button onClick={() => deleteTask(index)}> Delete</button>                
+            </li>
+            )}
+          </ul>
+        </Modal.Body>
+      </Modal>
     </main>
   );
 }
