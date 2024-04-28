a0584963127ae735adb2c47266f5e18228ad0bc5
Bryan Reiter <tul13138@temple.edu>
2024-04-28 18:00:35 -0400

    fixed some stuff that shouldnt be commented

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import requests
 import time
 import cv2
 import  asyncio, os
-# import mediapipe as mp
+import mediapipe as mp
 import time, math
 import numpy as np
 from methods import *
@@ -155,10 +155,10 @@ def processGesture(firstGesture, secondGesture=None):
 class VideoProcessor:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
-        # self.hands = mp.solutions.hands.Hands(static_image_mode=False,
-        #                                       max_num_hands=1,
-        #                                       min_detection_confidence=0.5,
-        #                                       min_tracking_confidence=0.5)
+        self.hands = mp.solutions.hands.Hands(static_image_mode=False,
+                                              max_num_hands=1,
+                                              min_detection_confidence=0.5,
+                                              min_tracking_confidence=0.5)
         self.latest_gesture = 'No gesture detected yet'
         self.firstGesture = 'No gesture detected'
         self.secondGesture = 'No gesture detected'
@@ -264,11 +264,11 @@ class VideoProcessor:
 app = Flask(__name__)
 CORS(app)
 #comment this out if mediapipe doesnt work
-# mpHands = mp.solutions.hands
-# hands = mpHands.Hands(static_image_mode=False,
-#                     max_num_hands=1,
-#                     min_detection_confidence=0.5,
-#                     min_tracking_confidence=0.5)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                    max_num_hands=1,
+                    min_detection_confidence=0.5,
+                    min_tracking_confidence=0.5)
 
 #until here
 #comment the next line in if mediapipe doesn't work
